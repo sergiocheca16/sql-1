b0158a8899ce03068a80b6da51133b9efd240796
Carlos <carlosdiazgirol@hotmail.com>
2024-02-02 12:05:32 +0100

    Ready to work

--- statements.sql
+++ statements.sql
@@ -1,119 +1,159 @@
 -- Ejercicio 1: Seleccionar todos los usuarios y sus lenguajes asociados.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 2: Contar cuántos usuarios hay en total.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 3: Seleccionar los usuarios mayores de 30 años.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 4: Contar cuántos usuarios saben JavaScript.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 5: Seleccionar los usuarios que tienen al menos 2 lenguajes asociados.
--- Tu consulta aquí
+
+-- Ejercicio 5 OPCION 2: Seleccionar los usuarios que tienen 28 años. 
+-- Tu respuesta aquí
+
 
 -- Ejercicio 6: Encontrar el usuario con el email 'juan.gomez@example.com'.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 7: Seleccionar los usuarios que saben Java y tienen menos de 25 años.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 8: Contar cuántos lenguajes diferentes saben los usuarios llamados 'Maria'.
--- Tu consulta aquí
+
+-- Ejercicio 8 OPCION 2: Contar cuántos usuarios con edades diferentes saben el lenguaje 'Java'.
+-- Tu respuesta aquí
+
 
 -- Ejercicio 9: Seleccionar los usuarios que no saben ningún lenguaje.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 10: Encontrar el usuario más joven que sabe al menos un lenguaje.
--- Tu consulta aquí
+
+-- Ejercicio 10: Encontrar el nombre y edad del usuario más joven.
+-- Tu respuesta aquí
+
 
 -- Ejercicio 11: Seleccionar los usuarios y sus edades ordenados por edad de forma descendente.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 12: Contar cuántos usuarios saben al menos un lenguaje y tienen más de 28 años.
--- Tu consulta aquí
+
+-- Ejercicio 12: Contar cuántos usuarios tienen más de 28 años.
+-- Tu respuesta aquí
+
 
 -- Ejercicio 13: Seleccionar los usuarios cuyo apellido contiene la letra 'a'.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 14: Encontrar el lenguaje más popular entre los usuarios menores de 30 años.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 15: Seleccionar los usuarios que saben al menos 3 lenguajes.
--- Tu consulta aquí
+
+-- Ejercicio 15: Seleccionar el usuario  mayor de 25 y que sepa el lenguaje 'TypeScript'.
+-- Tu respuesta aquí
+
 
 -- Ejercicio 16: Contar cuántos usuarios tienen un lenguaje asociado llamado 'Python'.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 17: Seleccionar los usuarios y sus lenguajes asociados, si tienen alguno, ordenados por nombre de usuario.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 18: Encontrar el usuario con el email que contiene la palabra 'example'.
--- Tu consulta aquí
+
+-- Ejercicio 18: Encontrar los usuario con el email que contiene la palabra 'example'.
+-- Tu respuesta aquí
+
 
 -- Ejercicio 19: Seleccionar los usuarios que saben al menos un lenguaje y tienen una edad entre 25 y 35 años.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 20: Contar cuántos usuarios tienen un lenguaje asociado llamado 'CSS' y tienen menos de 30 años.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 21: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar la cantidad de lenguajes que tienen.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 22: Encontrar el lenguaje más antiguo que conocen los usuarios mayores de 30 años.
--- Tu consulta aquí
+
+-- Ejercicio 22: Encontrar el lenguaje con más caracteres.
+-- Tu respuesta aquí
+
 
 -- Ejercicio 23: Seleccionar los usuarios y mostrar la concatenación de su nombre y apellido.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 24: Contar cuántos lenguajes diferentes conocen los usuarios mayores de 25 años.
--- Tu consulta aquí
 
--- Ejercicio 25: Seleccionar los usuarios que tienen exactamente dos lenguajes asociados.
--- Tu consulta aquí
+-- Ejercicio 24: Contar cuántos lenguajes diferentes conocen los usuarios mayores de 25 años. 
+-- Tu respuesta aquí
 
--- Ejercicio 26: Encontrar el usuario con el mayor número de lenguajes asociados.
--- Tu consulta aquí
+
+-- Ejercicio 25: Seleccionar los usuarios que tienen exactamente la misma edad.
+-- Tu respuesta aquí
+
+
+-- Ejercicio 26: Encontrar el usuario con el lenguaje con mayor número de carácteres y que tenga una edad menor de 30 años. 
+-- Tu respuesta aquí
+
 
 -- Ejercicio 27: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar sus emails.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
 
 -- Ejercicio 28: Contar cuántos usuarios tienen un apellido que comienza con la letra 'G'.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 29: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar el lenguaje más reciente que conocen.
--- Tu consulta aquí
 
--- Ejercicio 30: Encontrar el usuario con la edad más alta que sabe al menos un lenguaje.
--- Tu consulta aquí
+-- Ejercicio 29: Seleccionar los usuarios que tienen lenguajes que empiecen por 'J' y mostrar solo el que tiene menos caracteres. 
+-- Tu respuesta aquí
 
--- Ejercicio 31: Seleccionar los usuarios que saben al menos un lenguaje y ordenarlos por nombre de lenguaje.
--- Tu consulta aquí
 
--- Ejercicio 32: Contar cuántos usuarios tienen una edad entre 20 y 25 años y saben al menos un lenguaje.
--- Tu consulta aquí
+-- Ejercicio 30: Encontrar el usuario con edad mayor a 30 y que sabe el lenguaje con menos caracteres.
+-- Tu respuesta aquí
+
+
+-- Ejercicio 31: Seleccionar los usuarios que saben al menos un lenguaje y ordenarlos por nombre de lenguaje. 
+-- Tu respuesta aquí
+
+
+-- Ejercicio 32: Contar cuántos usuarios tienen una edad entre 20 y 25 años y saben al menos un lenguaje. 
+-- Tu respuesta aquí
+
 
 -- Ejercicio 33: Seleccionar los usuarios que no tienen un lenguaje asociado llamado 'SQL'.
--- Tu consulta aquí
+-- Tu respuesta aquí
 
--- Ejercicio 34: Encontrar el lenguaje más popular entre los usuarios que tienen al menos 30 años.
--- Tu consulta aquí
 
--- Ejercicio 35: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar la diferencia entre su edad y la edad promedio de todos los usuarios.
--- Tu consulta aquí
+-- Ejercicio 34: Encontrar el lenguaje con más caracteres entre los usuarios que tienen al menos 30 años.
+-- Tu respuesta aquí
 
--- Ejercicio 36: Contar cuántos usuarios tienen un lenguaje asociado que contiene la letra 'Script'.
--- Tu consulta aquí
 
--- Ejercicio 37: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar la longitud de su nombre.
--- Tu consulta aquí
+-- Ejercicio 35: Seleccionar los usuarios y mostrar la diferencia entre su edad y la edad promedio de todos los usuarios
+-- Tu respuesta aquí
 
--- Ejercicio 38: Encontrar el lenguaje más antiguo entre los usuarios que tienen exactamente un lenguaje asociado.
--- Tu consulta aquí
 
--- Ejercicio 39: Seleccionar los usuarios que saben al menos un lenguaje y mostrar la suma de sus edades.
--- Tu consulta aquí
+-- Ejercicio 36: Contar cuántos usuarios tienen un lenguaje asociado que contiene la palabra 'Script'.
+-- Tu respuesta aquí
+
+
+-- Ejercicio 37: Seleccionar los usuarios que tienen al menos un lenguaje asociado y mostrar la longitud de su nombre. 
+-- Tu respuesta aquí
+
+
+-- Ejercicio 38: Encontrar el lenguaje del usuario creado más tarde. 
+-- Tu respuesta aquí
+
+
+-- Ejercicio 39: Seleccionar los usuarios y mostrar la suma de sus edades. 
+-- Tu respuesta aquí
+
 
 -- Ejercicio 40: Contar cuántos usuarios tienen un lenguaje asociado que comienza con la letra 'P' y tienen menos de 28 años.
--- Tu consulta aquí
+-- Tu respuesta aquí
+
